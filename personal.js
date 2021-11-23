067a13d3213ec447d14f28b35380043d6668e987
jacob-hawkins <jacob.hawkins010@gmail.com>
2021-11-23 15:18:38 -0500

    update

--- personal.js
+++ personal.js
@@ -1,4 +1,5 @@
 var open = false;
+var cOpen = false;
 
 function openNav() {
     if (open == false) {
@@ -16,16 +17,18 @@ function closeNav() {
 }
 
 function openCard() {
-    if (open == false) {
-        document.getElementById("aboutCard").style.width = "250px";
-        open = true;
+    if (cOpen == false) {
+        document.getElementById("aboutCard").style.width = "87vw";
+        document.body.style.overflow = "hidden";
+        cOpen = true;
     }
 }
 
 function closeCard() {
-    if (open == true) {
-
+    if (cOpen == true) {
         document.getElementById("aboutCard").style.width = "0";
-        open = false;
+        document.body.style.overflow = "scroll";
+        console.log("overflow: visable");
+        cOpen = false;
     }
 }
